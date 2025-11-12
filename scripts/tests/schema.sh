902e54c077946299c2d11b01f00bdfa736030df8
williamjameswillis <william.willis1@nhs.net>
2025-11-12 16:01:32 +0000

    [GPCAPIM-204]:merge test coverage

--- scripts/tests/schema.sh
+++ scripts/tests/schema.sh
@@ -7,5 +7,10 @@ cd "$(git rev-parse --show-toplevel)"
 cd gateway-api
 mkdir -p test-artifacts
 poetry run pytest tests/schema/ -v \
+  --cov=src/gateway_api \
+  --cov-report=html:test-artifacts/coverage-html \
+  --cov-report=term \
   --junit-xml=test-artifacts/schema-tests.xml \
   --html=test-artifacts/schema-tests.html --self-contained-html
+# Save coverage data file for merging
+mv .coverage test-artifacts/.coverage.schema
