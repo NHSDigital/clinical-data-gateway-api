e7884d096a315a84db364b478bf3eaa42b3a3b3e
williamjameswillis <william.willis1@nhs.net>
2025-11-14 11:22:43 +0000

    [GPCAPIM-204]:include integration in coverage

--- scripts/tests/integration.sh
+++ scripts/tests/integration.sh
@@ -6,4 +6,6 @@ cd "$(git rev-parse --show-toplevel)"
 
 cd gateway-api
 mkdir -p test-artifacts
-poetry run behave --junit --junit-directory test-artifacts -f behave_html_pretty_formatter:PrettyHTMLFormatter -o test-artifacts/integration-tests.html
+poetry run coverage run -m behave --junit --junit-directory test-artifacts -f behave_html_pretty_formatter:PrettyHTMLFormatter -o test-artifacts/integration-tests.html
+# Save coverage data file for merging
+mv .coverage test-artifacts/coverage.integration
