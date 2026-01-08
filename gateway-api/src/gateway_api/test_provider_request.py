19453b90852c55f7172c67571a5dd827dc5b4079
DWolfsNHS <229101201+DWolfsNHS@users.noreply.github.com>
2026-01-08 09:14:17 +0000

    [GPCAPIM-251] initial commit with basic file structure and descriptions

---
+++ gateway-api/src/gateway_api/test_provider_request.py
@@ -0,0 +1,10 @@
+# makes valid requests to stub provider and checks responses
+
+# recieved 200 OK for valid request
+
+# (throws if not 200 OK)
+
+# returns what is received from stub provider (if valid)
+
+# ~~throws if invalid response from stub provider~~
+
