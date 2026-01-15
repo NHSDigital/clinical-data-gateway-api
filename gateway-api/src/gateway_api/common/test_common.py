76ee8f2bfb7b52f3696abf3638dc9ceb315ad046
Ben Taylor <59649826+Vox-Ben@users.noreply.github.com>
2026-01-15 15:52:23 +0000

    Change return type to flask response

---
+++ gateway-api/src/gateway_api/common/test_common.py
@@ -0,0 +1,29 @@
+# tests/test_common.py
+
+import pytest
+from src.gateway_api.common import common
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        "9434765919",
+        "943 476 5919",  # spaces allowed (non-digits stripped)
+        9434765919,  # int input supported
+    ],
+)
+def test_validate_nhs_number_valid(value):
+    assert common.validate_nhs_number(value) is True
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        "",  # empty
+        "123",  # too short
+        "12345678901",  # too long
+        "abc",  # no digits after stripping
+    ],
+)
+def test_validate_nhs_number_invalid_length_or_non_numeric(value):
+    assert common.validate_nhs_number(value) is False
