8d34def3f465ed046577f9223b642b9c11cce116
David Hamill <109090521+davidhamill1-nhs@users.noreply.github.com>
2026-01-21 16:58:42 +0000

    [GPCAPIM-254]: Correct module name.

--- gateway-api/src/gateway_api/get_structed_record/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Get Structured Record module."""
-
-from gateway_api.get_structed_record.handler import GetStructuredRecordHandler
-from gateway_api.get_structed_record.request import GetStructuredRecordRequest
-
-__all__ = ["GetStructuredRecordHandler", "GetStructuredRecordRequest"]
