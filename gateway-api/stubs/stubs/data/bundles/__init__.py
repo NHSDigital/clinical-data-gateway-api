a53bdcae8d4cd4795cc79fc0413a09c00c50f9fc
David Hamill <109090521+davidhamill1-nhs@users.noreply.github.com>
2026-02-23 15:01:06 +0000

    [GPCAPIM-275]: Improve layout of module

--- gateway-api/stubs/stubs/data/bundles/__init__.py
+++ gateway-api/stubs/stubs/data/bundles/__init__.py
@@ -1,20 +1,3 @@
-from typing import Any
+from stubs.data.bundles.bundles import Bundles
 
-from stubs.data.patients import Patients
-
-
-class Bundles:
-    @staticmethod
-    def _wrap_patient_in_bundle(patient: dict[str, Any]) -> dict[str, Any]:
-        return {
-            "resourceType": "Bundle",
-            "type": "collection",
-            "meta": {
-                "profile": [
-                    "https://fhir.nhs.uk/STU3/StructureDefinition/GPConnect-StructuredRecord-Bundle-1"
-                ]
-            },
-            "entry": [{"resource": patient}],
-        }
-
-    ALICE_JONES_9999999999 = _wrap_patient_in_bundle(Patients.ALICE_JONES_9999999999)
+__all__ = ["Bundles"]
--- gateway-api/stubs/stubs/data/patients/__init__.py
+++ gateway-api/stubs/stubs/data/patients/__init__.py
@@ -1,28 +1,3 @@
-import json
-import pathlib
-from typing import Any
+from stubs.data.patients.patients import Patients
 
-
-def _path_to_here() -> pathlib.Path:
-    return pathlib.Path(__file__).parent
-
-
-class Patients:
-    @staticmethod
-    def load_patient(filename: str) -> dict[str, Any]:
-        with open(_path_to_here() / filename, encoding="utf-8") as f:
-            patient: dict[str, Any] = json.load(f)
-        return patient
-
-    JANE_SMITH_9000000009 = load_patient("jane_smith_9000000009.json")
-    NO_SDS_RESULT_9000000010 = load_patient("no_sds_result_9000000010.json")
-    BLANK_ASID_SDS_RESULT_9000000011 = load_patient(
-        "blank_asid_sds_result_9000000011.json"
-    )
-    INDUCE_PROVIDER_ERROR_9000000012 = load_patient(
-        "induce_provider_error_9000000012.json"
-    )
-    BLANK_ENDPOINT_SDS_RESULT_9000000013 = load_patient(
-        "blank_endpoint_sds_result_9000000013.json"
-    )
-    ALICE_JONES_9999999999 = load_patient("alice_jones_9999999999.json")
+__all__ = ["Patients"]
