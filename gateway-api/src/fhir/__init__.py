f923cf07a72ed48bbefa3cb03aec2c824ccc594d
David Hamill <109090521+davidhamill1-nhs@users.noreply.github.com>
2026-03-11 23:29:07 +0000

    Restructure files.

--- gateway-api/src/fhir/__init__.py
+++ gateway-api/src/fhir/__init__.py
@@ -1,15 +1,13 @@
 """FHIR data types and resources."""
 
-from .elements import Issue, IssueCode, IssueSeverity
-from .resources import (
-    Bundle,
-    Device,
-    Endpoint,
-    OperationOutcome,
-    Parameters,
-    Patient,
-    Resource,
-)
+from .elements.issue import Issue, IssueCode, IssueSeverity
+from .resources.bundle import Bundle
+from .resources.device import Device
+from .resources.endpoint import Endpoint
+from .resources.operation_outcome import OperationOutcome
+from .resources.parameters import Parameters
+from .resources.patient import Patient
+from .resources.resource import Resource
 
 __all__ = [
     "Bundle",
---
+++ gateway-api/src/fhir/resources/parameters.py
@@ -0,0 +1,20 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Annotated
+
+from pydantic import Field
+
+from ..elements.identifier import NHSNumberValueIdentifier
+from .resource import Resource
+
+
+class Parameters(Resource, resource_type="Parameters"):
+    """A FHIR R4 Parameters resource."""
+
+    @dataclass(frozen=True)
+    class Parameter(ABC):
+        """A FHIR R4 Parameter resource."""
+
+        valueIdentifier: Annotated[NHSNumberValueIdentifier, Field(frozen=True)]
+
+    parameter: Annotated[list[Parameter], Field(frozen=True)]
