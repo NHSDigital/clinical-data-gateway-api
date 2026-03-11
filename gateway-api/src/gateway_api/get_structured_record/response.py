a23d29adec4d5e5f4a932f8a85412ffc2c35f9d0
David Hamill <109090521+davidhamill1-nhs@users.noreply.github.com>
2026-03-11 15:07:32 +0000

    Apply review comment for PR 275 from JW - move response behaviour out of request class.

---
+++ gateway-api/src/gateway_api/get_structured_record/response.py
@@ -0,0 +1,50 @@
+import json
+from collections.abc import Mapping
+from typing import ClassVar
+
+from flask import Request, Response
+from requests import Response as HTTPResponse
+from requests.structures import CaseInsensitiveDict
+
+from gateway_api.common.error import AbstractCDGError
+
+# Access record structured interaction ID from
+# https://developer.nhs.uk/apis/gpconnect/accessrecord_structured_development.html#spine-interactions
+ACCESS_RECORD_STRUCTURED_INTERACTION_ID = (
+    "urn:nhs:names:services:gpconnect:fhir:operation:gpc.getstructuredrecord-1"
+)
+
+
+class GetStructuredRecordResponse:
+    INTERACTION_ID: ClassVar[str] = ACCESS_RECORD_STRUCTURED_INTERACTION_ID
+    RESOURCE: ClassVar[str] = "patient"
+    FHIR_OPERATION: ClassVar[str] = "$gpc.getstructuredrecord"
+
+    MIME_TYPE: ClassVar[str] = "application/fhir+json"
+
+    def __init__(self) -> None:
+        self._response_body: str | None = None
+        self._headers: Mapping[str, str] | None = None
+        self._status_code: int | None = None
+
+    def mirror_headers(self, request: Request) -> None:
+        self._headers = CaseInsensitiveDict(request.headers)
+
+    @property
+    def headers(self) -> Mapping[str, str] | None:
+        return self._headers
+
+    def add_provider_response(self, provider_response: HTTPResponse) -> None:
+        self._response_body = json.dumps(provider_response.json())
+        self._status_code = provider_response.status_code
+
+    def add_error_response(self, error: AbstractCDGError) -> None:
+        self._response_body = error.operation_outcome.model_dump_json()
+        self._status_code = error.status_code
+
+    def build(self) -> Response:
+        return Response(
+            response=self._response_body,
+            status=self._status_code,
+            mimetype=self.MIME_TYPE,
+        )
---
+++ gateway-api/src/gateway_api/get_structured_record/test_response.py
@@ -0,0 +1,84 @@
+import json
+from typing import Any
+from unittest.mock import Mock
+
+from flask import request
+from requests.structures import CaseInsensitiveDict
+
+from gateway_api.app import app
+from gateway_api.common.error import UnexpectedError
+from gateway_api.get_structured_record.response import GetStructuredRecordResponse
+
+
+class TestGetStructuredRecordResponse:
+    def test_mirror_headers_adds_request_headers_to_response(
+        self, valid_simple_request_payload: dict[str, Any]
+    ) -> None:
+        additional_headers = CaseInsensitiveDict(
+            {"first": "a header", "second": "another header"}
+        )
+
+        with app.test_request_context(
+            "/patient/$gpc.getstructuredrecord",
+            method="POST",
+            data=json.dumps(valid_simple_request_payload),
+            headers=additional_headers,
+        ):
+            response = GetStructuredRecordResponse()
+            response.mirror_headers(request)
+
+            assert response.headers is not None, (
+                "Expected headers to be set, but they were None"
+            )
+            assert response.headers == dict(request.headers), (
+                "Expected response headers to match request headers, but they did not"
+            )
+
+    def test_add_provider_response_adds_provider_response_body(
+        self, valid_simple_response_payload: dict[str, Any]
+    ) -> None:
+        provider_response = Mock()
+        provider_response.status_code = 200
+        provider_response.json.return_value = valid_simple_response_payload
+
+        response = GetStructuredRecordResponse()
+        response.add_provider_response(provider_response)
+
+        actual = response.build().json
+        assert actual == valid_simple_response_payload, (
+            "Actual response body did not match actual response body."
+        )
+
+    def test_add_provider_response_adds_200_status(
+        self, valid_simple_response_payload: dict[str, Any]
+    ) -> None:
+        provider_response = Mock()
+        provider_response.status_code = 200
+        provider_response.json.return_value = valid_simple_response_payload
+
+        response = GetStructuredRecordResponse()
+        response.add_provider_response(provider_response)
+
+        actual = response.build().status_code
+        assert actual == 200, f"Expected status code to be 200, but got {actual}"
+
+    def test_add_error_response_adds_error_response_body(self) -> None:
+        error = UnexpectedError(traceback="something broke")
+
+        response = GetStructuredRecordResponse()
+        response.add_error_response(error)
+
+        expected_response_body = {
+            "resourceType": "OperationOutcome",
+            "issue": [
+                {
+                    "severity": "error",
+                    "code": "exception",
+                    "diagnostics": "Internal Server Error: something broke",
+                }
+            ],
+        }
+        actual_response_body = response.build().json
+        assert actual_response_body == expected_response_body, (
+            "Actual response body did not match expected response body."
+        )
