53844427b1e7c8405479e3e4291fead8b4299d60
David Hamill <109090521+davidhamill1-nhs@users.noreply.github.com>
2026-03-11 15:59:17 +0000

    Remove unused TypedDict FHIR types.

--- gateway-api/src/fhir/__init__.py
+++ gateway-api/src/fhir/__init__.py
@@ -1,22 +1,9 @@
 """FHIR data types and resources."""
 
-from fhir.bundle import BundleEntry, BundleTypedDict
-from fhir.general_practitioner import GeneralPractitioner
-from fhir.human_name import HumanName
-from fhir.identifier import Identifier
-from fhir.operation_outcome import OperationOutcome, OperationOutcomeIssue
-from fhir.parameters import Parameter, ParametersTypedDict
-from fhir.patient import PatientTypedDict
+from .resources import OperationOutcome, Parameters, Patient
 
 __all__ = [
-    "BundleTypedDict",
-    "BundleEntry",
-    "HumanName",
-    "Identifier",
     "OperationOutcome",
-    "OperationOutcomeIssue",
-    "Parameter",
-    "ParametersTypedDict",
-    "PatientTypedDict",
-    "GeneralPractitioner",
+    "Parameters",
+    "Patient",
 ]
