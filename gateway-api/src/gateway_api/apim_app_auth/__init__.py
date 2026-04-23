561ac317c4b947a028667d3d7a76c57837d3e6e8
DWolfsNHS <229101201+DWolfsNHS@users.noreply.github.com>
2026-04-23 14:26:40 +0000

    [GPCAPIM-395]: Remove unused imports from APIM auth module

--- gateway-api/src/gateway_api/apim_app_auth/__init__.py
+++ gateway-api/src/gateway_api/apim_app_auth/__init__.py
@@ -4,11 +4,8 @@ from gateway_api.apim_app_auth.apim import (
     ApimAuthenticationException,
     ApimAuthenticator,
 )
-from gateway_api.apim_app_auth.http import RequestMethod, SessionManager
 
 __all__ = [
     "ApimAuthenticationException",
     "ApimAuthenticator",
-    "RequestMethod",
-    "SessionManager",
 ]
