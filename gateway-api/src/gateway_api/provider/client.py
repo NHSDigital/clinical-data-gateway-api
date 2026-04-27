ae1e1612dc4536253300aebdd84b4126d6220e94
David Hamill <109090521+davidhamill1-nhs@users.noreply.github.com>
2026-04-27 23:43:34 +0100

    Add body to provider response logging, in order to understand/confirm errors.

--- gateway-api/src/gateway_api/provider/client.py
+++ gateway-api/src/gateway_api/provider/client.py
@@ -145,6 +145,7 @@ class GpProviderClient:
         log_details = {
             "description": "GPProvider FHIR API response received",
             "status_code": str(response.status_code),
+            "body": response.text,  # TODO: Remove me before putting up for review
         }
         _logger.info(log_details)
 
